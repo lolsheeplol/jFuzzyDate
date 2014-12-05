952a52e73c85669c427e6cda0d08a1d2cdeaa74e
Ralf Schwach <cmmrasch@users.noreply.github.com>
2014-12-05 07:54:44 +0000

    java version 1.6 & fallback on missing key in bundle

--- src/main/java/net/sf/jfuzzydate/i18n/ResourceBundleFSProvider.java
+++ src/main/java/net/sf/jfuzzydate/i18n/ResourceBundleFSProvider.java
@@ -1,3 +1,13 @@
+/*
+ * Copyright 2005-2014 chemmedia AG
+ *
+ * You should have received a copy of a license with this program. If not,
+ * contact us by visiting http://www.chemmedia.de/ or write to chemmedia AG,
+ * Parkstraße 35, 09120 Chemnitz, Germany.
+ *
+ * You may not use, copy, modify, sublicense, or distribute the Program or any
+ * portion of it, except as expressly provided under the given license.
+ */
 package net.sf.jfuzzydate.i18n;
 
 import net.sf.jfuzzydate.FuzzyStringProvider;
@@ -5,6 +15,7 @@ import net.sf.jfuzzydate.FuzzyStringProvider;
 import java.text.MessageFormat;
 
 import java.util.Locale;
+import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 
@@ -14,21 +25,22 @@ import java.util.ResourceBundle;
  * @author amaasch
  */
 public class ResourceBundleFSProvider implements FuzzyStringProvider {
+
     //~ Instance fields --------------------------------------------------------------------------------------
 
     /**
-     * Java {@link ResourceBundle} base name. The bundle at this name contains the keys for this
-     * string builder instance.
+     * Java {@link ResourceBundle} base name. The bundle at this name contains the keys for this string
+     * builder instance.
      */
     private final String bundleBaseName;
 
     //~ Constructors -----------------------------------------------------------------------------------------
 
-/**
+    /**
      * Creates a new fuzzy string builder instance using a java resource bundle.
      *
-     * @param fuzzyStringBundle the base name of the bundle that contains the 
-     *        keys for this fuzzy string builder. 
+     * @param fuzzyStringBundle the base name of the bundle that contains the keys for this fuzzy string
+     *                          builder.
      */
     public ResourceBundleFSProvider(final String fuzzyStringBundle) {
         bundleBaseName = fuzzyStringBundle;
@@ -49,7 +61,13 @@ public class ResourceBundleFSProvider implements FuzzyStringProvider {
     public String getString(final String key, final Locale locale) {
         final ResourceBundle bundle = ResourceBundle.getBundle(bundleBaseName, locale);
 
-        return bundle.getString(key);
+        try {
+            return bundle.getString(key);
+        } catch (final MissingResourceException e) {
+            final ResourceBundle fallbackBundle = ResourceBundle.getBundle(bundleBaseName, Locale.ROOT);
+
+            return fallbackBundle.getString(key);
+        }
     }
 
     /* (non-Javadoc)
